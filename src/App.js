ba4f63ba7aa26f6862a4a8dc7e8a3b2bde8faf43
simphiweqwina <simphiweqwina@gmail.com>
2020-08-11 18:09:40 +0200

    added lambda functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    <h1>You are now logged in</h1>
-  </div>
-);
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+console.log(awsconfig)
+
+
+const App = () => {
+
+  const [usernames, setUsernames] = useState([])
+
+  axios({
+    url: 'https://dcd5w0pq9k.execute-api.us-east-1.amazonaws.com/enveldev/envelGetUsers',
+    method: 'get'
+  })
+    .then(response => {
+      console.log(response.data)
+      setUsernames(response.data)
+      console.log(this.state.usernames)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
+
+
+
+
+  return (
+    < div className="container" >
+      < AmplifySignOut />
+      <h1>Hi {Auth.user.username}</h1>
+
+
+      <h3>Other users</h3>
+      <p>{console.log(usernames)}</p>
+
+
+      {
+        usernames.map((student, index) => (
+          <tr data-index={index}>
+            <td>{student.username}</td>
+          </tr>
+        ))
+      }
+    </div >
+  );
+
+
+};
 
 export default withAuthenticator(App);
